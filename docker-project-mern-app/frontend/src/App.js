36e7b0fc0cdf781c3bf8b1b2d6224ddf1e85a957
root <root@Hari.localdomain>
2025-11-16 21:02:49 +0000

    updated

---
+++ docker-project-mern-app/frontend/src/App.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import './App.css';
+import { UserContext } from './components/contexts/UserContext';
+import CreateEmployee from './components/createemployee';
+import Dashboard from './components/dashboard';
+import EditEmployee from './components/editemployee';
+import EmployeeTable from './components/employee';
+import Login from './components/login';
+import Private from './components/private';
+import Register from "./components/Register";
+
+function App() {
+  const [loggedUser,setLoggedUser]=useState(localStorage.getItem('token'));
+  useEffect(()=>{
+    console.log(loggedUser)
+
+  })
+  return (
+    <UserContext.Provider value={{loggedUser,setLoggedUser}}>
+    <Router>
+      <div className="App">
+        <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Private Component={ Dashboard} />} />
+          <Route path="/employees" element={<Private Component={EmployeeTable} />} />
+          <Route path="/edit-employee/:id" element={<Private Component= {EditEmployee} />} />
+          <Route path="/create-employee" element={<Private Component ={CreateEmployee} />} />
+        </Routes>
+      </div>
+    </Router>
+    </UserContext.Provider>
+  );
+}
+
+export default App;
