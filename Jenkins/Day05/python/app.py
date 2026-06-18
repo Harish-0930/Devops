e2ab23d732b129eee7dd4d563ba33ef547061850
Munagala Harish <munagalaharish2002@gmail.com>
2026-06-18 21:20:03 +0530

    updated app mutlibranch

---
+++ Jenkins/Day05/python/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.get("/")
+def hello():
+  return jsonify(
+    message="Welcome to flask application",
+    tip="Built with Flask, shipped by Jenkins, running in Docker.",
+    UI="This is my new UI, which is built with React and deployed in Docker.UI has been modified again."
+  )
+if __name__ == "__main__":
+  app.run(host="0.0.0.0",port=5000)
